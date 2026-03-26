5848a2998e76d23c803920de67062e89aee10460
guhzzkz <gugustafrts@gmail.com>
2026-03-26 12:01:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const menuButton = document.querySelector('.mobile-menu-button');
+const nav = document.querySelector('#main-nav');
 
+if (menuButton && nav) {
+  menuButton.addEventListener('click', () => {
+    const isOpen = nav.classList.toggle('open');
+    menuButton.setAttribute('aria-expanded', String(isOpen));
+  });
+}
